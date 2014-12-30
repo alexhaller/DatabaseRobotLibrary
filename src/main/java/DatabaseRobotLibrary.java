11d5681ffe347dbb148927c53cfa47c42f2597b0
alexhaller <alexanderhaller90@gmail.com>
2014-12-30 09:29:54 +0100

    Refactored DatabaseRobotLibrary and added ROBOT_LIBRARY_DOC_FORMAT

--- src/main/java/DatabaseRobotLibrary.java
+++ src/main/java/DatabaseRobotLibrary.java
@@ -1,10 +1,14 @@
 import org.robotframework.javalib.library.AnnotationLibrary;
 
 public class DatabaseRobotLibrary extends AnnotationLibrary {
-    public static final String ROBOT_LIBRARY_SCOPE = "TEST SUITE";
-    public static final String ROBOT_LIBRARY_VERSION = "1.0";
-    
-    public DatabaseRobotLibrary() {
-        super("org/robotframework/databaserobotlibrary/DatabaseKeywords.class");
-    }
+	// Reference: http://robotframework.googlecode.com/hg/doc/userguide/RobotFrameworkUserGuide.html
+	// Possible values: "TEST CASE", "TEST SUITE", "GLOBAL"
+	public static final String ROBOT_LIBRARY_SCOPE = "TEST CASE";
+	public static final String ROBOT_LIBRARY_VERSION = "1.0";
+	public static final String ROBOT_LIBRARY_DOC_FORMAT = "HTML";
+	public static final String KEYWORD_PATTERN = "org/robotframework/databaserobotlibrary/**/*.class";
+
+	public DatabaseRobotLibrary() {
+		super(KEYWORD_PATTERN);
+	}
 }
