5604c7f2126617744f65ff3a61177aecf20b52fb
Alexander Haller <info@alexanderhaller.de>
2017-01-07 14:13:22 +0100

    Updated comments

--- src/main/java/DatabaseRobotLibrary.java
+++ src/main/java/DatabaseRobotLibrary.java
@@ -1,7 +1,7 @@
 import org.robotframework.javalib.library.AnnotationLibrary;
 
+//Reference: http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html
 public class DatabaseRobotLibrary extends AnnotationLibrary {
-	// Reference: http://robotframework.googlecode.com/hg/doc/userguide/RobotFrameworkUserGuide.html
 	// Possible values: "TEST CASE", "TEST SUITE", "GLOBAL"
 	public static final String ROBOT_LIBRARY_SCOPE = "TEST CASE";
 	public static final String ROBOT_LIBRARY_VERSION = "1.0";
